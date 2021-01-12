774519e3e405fa2bb00329fb6b30e5794f2d37ce
rezabojnordi <rezabojnordi2012@gmail.com>
2021-01-12 11:25:35 +0330

    dashboard prometheus

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PROMETHEUS_VERSION="2.21.0"
+PROMETHEUS_VERSION="2.24.0"
 wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 tar -xzvf prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 cd prometheus-${PROMETHEUS_VERSION}.linux-amd64/
