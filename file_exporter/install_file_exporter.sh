eda5a54130fd16586f8ba262ea84910e006460ad
AliEbrahimpour <ali74.ebrahimpour@gmail.com>
2022-03-02 09:17:47 +0330

    update user
    update user

--- file_exporter/install_file_exporter.sh
+++ file_exporter/install_file_exporter.sh
@@ -4,16 +4,16 @@ cp filestat_exporter /usr/local/bin/filestat_exporter
 cp filestat.yaml /usr/local/bin/filestat.yaml
 
 # create user
-useradd --no-create-home --shell /bin/false filestat_exporter
-chown filestat_exporter:filestat_exporter /usr/local/bin/filestat_exporter
+#useradd --no-create-home --shell /bin/false filestat_exporter
+#chown filestat_exporter:filestat_exporter /usr/local/bin/filestat_exporter
 
 echo '[Unit]
 Description=File Stat Exporter
 Wants=network-online.target
 After=network-online.target
 [Service]
-User=filestat_exporter
-Group=filestat_exporter
+User=root
+Group=root
 Type=simple
 ExecStart=/usr/local/bin/filestat_exporter  -config.file /usr/local/bin/filestat.yaml
 [Install]
