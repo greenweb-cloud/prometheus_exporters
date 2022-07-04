c5d105d2b80ef017d265cd9fb2aa3b1ad95b2845
AliEbrahimpour <ali74.ebrahimpour@gmail.com>
2022-07-04 15:34:32 +0430

    Update upgrade_prometheus.sh

--- prometheus/upgrade_prometheus.sh
+++ prometheus/upgrade_prometheus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#set new version from https://github.com/prometheus/prometheus
-PROMETHEUS_VERSION="2.36.2"
+#get new version from https://github.com/prometheus/prometheus
+PROMETHEUS_VERSION=$(curl -sL https://api.github.com/repos/prometheus/prometheus/releases/latest | grep "tag_name"   | sed -E 's/.*"([^"]+)".*/\1/'|sed 's/v//')
+
 wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 tar -xzvf prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 cd prometheus-${PROMETHEUS_VERSION}.linux-amd64/
@@ -14,10 +15,6 @@ cp promtool /usr/local/bin/
 
 chown prometheus:prometheus /usr/local/bin/prometheus
 chown prometheus:prometheus /usr/local/bin/promtool
-
-
-chown -R prometheus:prometheus /etc/prometheus/consoles
-chown -R prometheus:prometheus /etc/prometheus/console_libraries
 
 
 systemctl daemon-reload
