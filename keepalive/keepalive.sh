9fe6c41c6ef5de27c61545d7c8bd56ec1d41cfd8
aliebrahimpour <ali74.ebrahimpour@gmail.com>
2022-11-12 12:22:04 +0000

    change version exporter

--- keepalive/keepalive.sh
+++ keepalive/keepalive.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export VERSION="1.2.0"
+export VERSION=$(curl -sL https://api.github.com/repos/cafebazaar/keepalived-exporter/releases/latest | grep "tag_name"   | sed -E 's/.*"([^"]+)".*/\1/'|sed 's/v//')
+#export VERSION="1.2.0"
 wget https://github.com/cafebazaar/keepalived-exporter/releases/download/v${VERSION}/keepalived-exporter-${VERSION}.linux-amd64.tar.gz
 tar xvzf keepalived-exporter-${VERSION}.linux-amd64.tar.gz keepalived-exporter-${VERSION}.linux-amd64/keepalived-exporter
 #sudo mv keepalived-exporter-${VERSION}.linux-amd64/keepalived-exporter /usr/local/bin/
--- node_exporter/node-exporter.sh
+++ node_exporter/node-exporter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-NODE_EXPORTER_VERSION="1.3.1"
+NODE_EXPORTER_VERSION=$(curl -sL https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep "tag_name"   | sed -E 's/.*"([^"]+)".*/\1/'|sed 's/v//')
+#NODE_EXPORTER_VERSION="1.3.1"
 wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
 tar -xzvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
 cd node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64
